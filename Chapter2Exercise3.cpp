74026f88fe034bb4e632b13ba75df98b4e5d1b32
ctpride07 <ctpride07@gmail.com>
2025-09-04 13:33:25 -0400

    Add sales tax calculation and display functionality
    This commit introduces code to calculate and display the sales tax and total purchase amount based on a specified purchase amount. It includes variables for purchase amount, state and county sales tax, total sales tax, and total purchase amount, and outputs these values to the console.

--- Chapter2Exercise3.cpp
+++ Chapter2Exercise3.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 int main()
 {
-	
+	float purchaseAmount = 95.00;
+	float stateSalesTax = 0.04;
+	float countySalesTax = 0.02;
+	float totalSalesTax = stateSalesTax + countySalesTax;
+	float salesTaxAmount = purchaseAmount * totalSalesTax;
+	float totalPurchaseAmount = purchaseAmount + salesTaxAmount;
+
+	cout << "Purchase Amount: $" << purchaseAmount << endl;
+	cout << "State Sales Tax: $" << purchaseAmount * stateSalesTax << endl;
+	cout << "County Sales Tax: $" << purchaseAmount * countySalesTax << endl;
+	cout << "Total Sales Tax: $" << salesTaxAmount << endl;
+	cout << "Total Purchase Amount: $" << totalPurchaseAmount << endl;
 }
